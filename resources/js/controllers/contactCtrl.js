80b1da38668d0639a6c1560fc9b38f3fc56b9b1c
kbl212 <kyle.lauritzen@gmail.com>
2016-05-15 00:28:13 -0600

    job openings page done

--- resources/js/controllers/contactCtrl.js
+++ resources/js/controllers/contactCtrl.js
@@ -2,5 +2,23 @@ var app = angular.module('sunhorizonenergy');
 
 
 app.controller('contactCtrl', function($scope) {
-    
+        $scope.initMap = function () {
+        var myLatLng = {
+            lat: 40.288175,
+            lng: -111.693569
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 16,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: $scope.map,
+            title: 'sunHorizonEnergy'
+        });
+    }
+
+    $scope.initMap();
 })
---
+++ resources/php/contact.php
@@ -0,0 +1,16 @@
+<div id="contact-wrapper">
+    <div class="container">
+        <div class="our-location">
+            <div id="map-location">
+                <div id="map"></div>
+                <div id="address">487 South Orem Boulevard, Orem, UT, United States
+                </div>
+            </div>
+            <br>
+            <p></p>
+        </div>
+        <div class="get-in-touch">
+
+        </div>
+    </div>
+</div>
